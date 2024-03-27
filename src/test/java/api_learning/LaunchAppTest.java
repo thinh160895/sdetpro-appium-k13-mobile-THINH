d42543d488cc625a21a18bd5959fa79b877f8764
thuynhvan <thuynhvan@dotdash.com>
2024-03-27 07:31:19 -0500

    LaaunchApp class added

--- src/test/java/api_learning/LaunchAppTest.java
+++ src/test/java/api_learning/LaunchAppTest.java
@@ -1,5 +1,33 @@
 package api_learning;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class LaunchAppTest {
 
+  public static void main(String[] args) {
+    // DesiredCaps
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    desiredCapabilities.setCapability("platformName", "Android");
+    desiredCapabilities.setCapability("automationName", "uiautomator2");
+    desiredCapabilities.setCapability("udid", "emulator-5554");
+    desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+    desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+    AppiumDriver appiumDriver = null;
+    // Init session
+    try {
+      URL appiumServer = new URL("http://localhost:4723");
+      appiumDriver = new AndroidDriver(appiumServer, desiredCapabilities);
+    } catch (Exception e){
+      e.printStackTrace();
+    }
+
+    // Close session
+    if(appiumDriver != null) {
+      appiumDriver.quit();
+    }
+  }
 }
