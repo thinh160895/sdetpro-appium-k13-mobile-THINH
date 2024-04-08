f80ad9f5174b7256d60cc6aa9bc90c4766270a23
thuynhvan <thuynhvan@dotdash.com>
2024-04-08 08:26:10 -0500

    Day q8

--- src/test/java/api_learning/LoginFormInteraction.java
+++ src/test/java/api_learning/LoginFormInteraction.java
@@ -4,8 +4,11 @@ import driver.DriverFactory;
 import driver.Platform;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginFormInteraction {
 
@@ -30,12 +33,22 @@ public class LoginFormInteraction {
 //      By passwordLoc = By.xpath("//android.widget.EditText[@content-desc=\"input-password\"]");
       By passwordLoc = AppiumBy.accessibilityId("input-password");
       WebElement passwordEle = appiumDriver.findElement(passwordLoc);
-      passwordEle.sendKeys("987654321");
+      passwordEle.sendKeys("12345678");
 
       // Click on Login Btn
       By loginBtnLoc = AppiumBy.accessibilityId("button-LOGIN");
       WebElement loginBtnEle = appiumDriver.findElement(loginBtnLoc);
       loginBtnEle.click();
+
+      // Wait for the dialog displayed
+      By dialogMsgLoc = AppiumBy.id("android:id/message");
+      By dialogBtnLoc = AppiumBy.id("android:id/button1");
+
+      // Using explicit wait
+      WebDriverWait wait = new WebDriverWait(appiumDriver, Duration.ofSeconds(15));
+      WebElement dialogMsgEle = wait.until(ExpectedConditions.visibilityOfElementLocated(dialogMsgLoc));
+      System.out.printf("Dialog msg: %s\n", dialogMsgEle.getText());
+      appiumDriver.findElement(dialogBtnLoc).click();
 
     } catch (Exception e) {
       e.printStackTrace();
---
+++ src/test/java/api_learning/SwipeVertically.java
@@ -0,0 +1,82 @@
+package api_learning;
+
+import driver.DriverFactory;
+import driver.Platform;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import java.time.Duration;
+import java.util.Collections;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.PointerInput.Kind;
+import org.openqa.selenium.interactions.PointerInput.MouseButton;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SwipeVertically {
+
+  public static void main(String[] args) {
+    AppiumDriver appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+    try {
+      By formsBtnLoc = AppiumBy.accessibilityId("Forms");
+      By activeBtnLoc = AppiumBy.accessibilityId("button-Active");
+
+      // Navigate to [Forms] screen
+      appiumDriver.findElement(formsBtnLoc).click();
+
+      // Make sure we are on the target screen before swiping up/down/left/right/any direction
+      WebDriverWait wait = new WebDriverWait(appiumDriver, Duration.ofSeconds(15L));
+      wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.androidUIAutomator("new UiSelector(). textContains(\"Form components\")")));
+
+      // Swipe up before interacting
+      Dimension windowSize = appiumDriver.manage().window().getSize();
+      int screenHeight = windowSize.getHeight();
+      int screenWidth = windowSize.getWidth();
+      System.out.printf("%d x %d", screenWidth, screenHeight);
+
+      // Construct coordinators
+      int startX = 50 * screenWidth / 100;
+      int startY = 50 * screenHeight / 100;
+      int endX = startX;
+      int endY = 10 * screenHeight / 100;
+
+      // Specify PointerInput as [TOUCH] with name [finger1]
+      PointerInput pointerInput = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+
+      // Specify sequence
+      Sequence sequence = new Sequence(pointerInput, 1)
+          .addAction(pointerInput.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+          .addAction(pointerInput.createPointerDown(MouseButton.LEFT.asArg()))
+          .addAction(new Pause(pointerInput, Duration.ofMillis(250)))
+          .addAction(pointerInput.createPointerMove(Duration.ofMillis(250), PointerInput.Origin.viewport(), endX, endY))
+          .addAction(pointerInput.createPointerUp(MouseButton.LEFT.asArg()));
+
+      // Ask appium server to perform the sequence
+      appiumDriver.perform(Collections.singletonList(sequence));
+
+      // Interact with element on the screen
+      appiumDriver.findElement(activeBtnLoc).click();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // DEBUG PURPOSE ONLY
+    try {
+      Thread.sleep(3000);
+    } catch (Exception ignored) {
+    }
+
+    appiumDriver.quit();
+  }
+
+  /*
+   * Locator | By | from AppiumBy
+   * Element | WebElement
+   *
+   * */
+
+}
