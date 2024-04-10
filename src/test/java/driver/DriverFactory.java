ea1ad35e629881de0d7ca5b732619290d5ee9d38
thinh160895 <thinh.tran@techsoft.asia>
2024-04-10 14:06:24 +0700

    Change dependence maven to 9.2.2 And change UDID of real device

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -16,7 +16,7 @@ public class DriverFactory {
     DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
     desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
     desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME_OPTION, "uiautomator2");
-    desiredCapabilities.setCapability(MobileCapabilityType.UDID_OPTION, "emulator-5554");
+    desiredCapabilities.setCapability(MobileCapabilityType.UDID_OPTION, "R39M10FTLFM");
     desiredCapabilities.setCapability(MobileCapabilityType.APP_PACKAGE_OPTION, "com.wdiodemoapp");
     desiredCapabilities.setCapability(MobileCapabilityType.APP_ACTIVITY_OPTION,
         "com.wdiodemoapp.MainActivity");
